5c36c77b173e28fe912bbc6be1ce8b9d70839ba3
rojaho <rjh420@gmail.com>
2024-10-04 08:53:11 +0100

    Create partThree.py

---
+++ partThree.py
@@ -0,0 +1,14 @@
+def main():
+    pounds = pounds_to_float(input("How much was the meal? "))
+    percent = percent_to_float(input("What percentage would you like to charge? "))
+    charge = pounds * percent
+    print(f"Charge £{charge:.2f}")
+
+
+def pounds_to_float(d):
+    # TODO
+
+def percent_to_float(p):
+    # TODO
+
+main()
